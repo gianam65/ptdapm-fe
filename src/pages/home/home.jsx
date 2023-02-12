b0c12fa50660d52d6209d1737f098f76223cb98a
ManhPhamGithub <ducmanhpham.work@gmail.com>
2023-02-12 18:04:41 +0700

    fix: test commit

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,7 +1,11 @@
 import './home.scss';
 
 const Home = () => {
-  return <div className="home__container">This is home page</div>;
+  return <div className="home__container">
+    This is home page
+    <h1>hello</h1>
+  </div>;
+
 };
 
 export default Home;
