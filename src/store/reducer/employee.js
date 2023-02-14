44f90c4c3d8e148caf71d3eda4153ab3c3778a94
gianam65 <gianam65012001@gmail.com>
2023-02-14 22:00:31 +0700

    fix warning, add primary color

--- src/store/reducer/employee.js
+++ src/store/reducer/employee.js
@@ -1,26 +1,20 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { GET_EMPLOYEE_REQUEST } from '../../actions/types/employeeType'
-
 
 const initialState = {
-    listEmployee: [],
-    status: 'idle'
+  listEmployee: [],
+  status: 'idle'
 };
 
 export const employeeReducer = createSlice({
-    name: 'employee',
-    initialState,
-    reducers: {
-        getEmployee: (state, action) => {
-            state.listEmployee = action.payload;
-        },
-        addEmployee: (state, action) => {
-
-        }
-    }
+  name: 'employee',
+  initialState,
+  reducers: {
+    getEmployee: (state, action) => {
+      state.listEmployee = action.payload;
+    },
+    addEmployee: (state, action) => {}
+  }
 });
 export const { getEmployee, addEmployee } = employeeReducer.actions;
 
 export default employeeReducer.reducer;
-
-
