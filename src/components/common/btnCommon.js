0b37dc23c81567d46f78735a114ec0c99b205e68
loi02112001 <loi65732102@gmail.com>
2023-02-16 14:16:21 +0700

    update property disable

--- src/components/common/btnCommon.js
+++ src/components/common/btnCommon.js
@@ -1,6 +1,6 @@
 export default function Button({size='md',className,children,onClick,icon,disable=false}){
     return(
-        <button className={className} onClick={onClick} disablgited>
+        <button className={className} onClick={onClick} disabledgit>
             {icon}
             {children}
         </button>   
