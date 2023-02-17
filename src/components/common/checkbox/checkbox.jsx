11e6b86612454a4ea8b5f92436516c4cd1838109
Pham Tuan Duc <89022216+toughguy1010@users.noreply.github.com>
2023-02-17 16:13:21 +0700

    add common checkbox

---
+++ src/components/common/checkbox/checkbox.jsx
@@ -0,0 +1,25 @@
+import Checkbox from '@mui/material/Checkbox';
+import classNames from 'classnames';
+import './checkbox.scss';
+
+export default function Checkboxes({ 
+  className,   
+  checked, 
+  disabled, 
+  defaultChecked, 
+  onChange, 
+  small = false,
+  large = false,
+  style,
+  ...props}) {
+  const classes = classNames('checkbox__default',{
+    [className] : className,
+    checkbox__small: small,
+    checkbox__large: large
+  })
+  return (
+    <div>
+      <Checkbox style={style} disabled={disabled} checked={checked} className={classes}   {...props} onChange={onChange}/>
+    </div>
+  );
+}
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,12 +1,16 @@
 import './home.scss';
 import Button from '../../components/common/button/button';
-const Home = () => {
+import Checkboxes from '../../components/common/checkbox/checkbox'
+  const Home = () => {
   return (
     <div className="home__container">
       This is home page
       <h1>hello</h1>
-      <Button className="hehehe">Test btn</Button>
+      <Button   className="hehehe">Test btn</Button>
+      <Checkboxes large />
+      
     </div>
+    
   );
 };
 
