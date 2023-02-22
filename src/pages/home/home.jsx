99e8c95a8a9f21a5310a8aafb289d2b1064e1802
loi02112001 <loi65732102@gmail.com>
2023-02-22 10:00:00 +0700

    update onClick for button

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,13 +1,23 @@
 import './home.scss';
 import Button from '../../components/common/button/button';
 import Checkboxes from '../../components/common/checkbox/checkbox'
+import Input from '../../components/common/input/input';
+import Notify from '../../components/common/nofity/notify';
+import { useState } from 'react';
   const Home = () => {
+    const [showNotify, setShowNotify] = useState(false)
+  const handleClose = ()=>{
+  setShowNotify(false)
+ }
   return (
     <div className="home__container">
       This is home page
       <h1>hello</h1>
-      <Button className="hehehe">Test btn</Button>
+      <Button className="hehehe" onClick={()=>{setShowNotify(true)}}>Test btn</Button>
       <Checkboxes large />
+      <Input large/>
+      <Notify showNotify={showNotify} onCloseNotify={handleClose} autoHideDuration={3000} severity={"success"} message={'add thanh cong'}/>
+      {/* severity: success || error || warning*/}
     </div>
   );
 };
