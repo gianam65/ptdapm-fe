40025be6bafb8d67223b6320644708481a67c027
gianam65 <gianam65012001@gmail.com>
2023-02-22 22:51:36 +0700

    add default layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { publicRoutes } from './routes';
+import PageLayout from './layout';
 
 function App() {
   const publicRouter = createBrowserRouter(publicRoutes);
-  return <RouterProvider router={publicRouter} />;
+  return (
+    <PageLayout>
+      <RouterProvider router={publicRouter} />
+    </PageLayout>
+  );
 }
 
 export default App;
--- src/layout/index.js
+++ src/layout/index.js
@@ -1,1 +1,11 @@
-//  test commit 
+import './index.scss';
+const PageLayout = ({ children }) => {
+  return (
+    <div className="default__layout-container">
+      <div className="menu__side">Test menu</div>
+      <div className="page__content">{children}</div>
+    </div>
+  );
+};
+
+export default PageLayout;
