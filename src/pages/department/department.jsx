9b7f4ce7dad21dde5a9fbcc0d3941a1373531cd6
gianam65 <gianam65012001@gmail.com>
2023-02-28 22:36:54 +0700

    add feat upsert department

--- src/pages/department/department.jsx
+++ src/pages/department/department.jsx
@@ -3,20 +3,23 @@ import { PlusOutlined, MoreOutlined, EditOutlined, DeleteOutlined, ExclamationCi
 import Button from '../../components/button/button';
 import { Modal, Table, Popover, notification } from 'antd';
 import { getAPIHostName } from '../../utils/';
-import { httpGet, httpDelete } from '../../services/request';
-import { useEffect, useState } from 'react';
+import { httpGet, httpDelete, httpPost, httpPut } from '../../services/request';
+import { useEffect, useState, useRef } from 'react';
 import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { accessTokenState } from '../../recoil/store/account';
 import { loadingState } from '../../recoil/store/app';
 import CustomInput from '../../components/custom-input/custom-input';
 
-const { confirm } = Modal;
 export default function DepartmentPage() {
   const accessToken = useRecoilValue(accessTokenState);
   const [departmentList, setDepartmentList] = useState([]);
   const [searchValue, setSearchValue] = useState('');
   const [isLoadingTable, setIsLoadingTable] = useState(false);
+  const [openUpSertDepartment, setOpenUpSertDepartment] = useState(false);
+  const [updateId, setUpdateId] = useState();
   const setPageLoading = useSetRecoilState(loadingState);
+  const departmentNameRef = useRef(null);
+  const departmentCodeRef = useRef(null);
   useEffect(() => {
     const getDepartment = () => {
       const url = `${getAPIHostName()}/departments`;
@@ -42,12 +45,44 @@ export default function DepartmentPage() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const openModalUpSertDepartment = id => {
+    setUpdateId(id);
+    setOpenUpSertDepartment(true);
+  };
+
+  const handleUpdateDepartment = id => {
+    if (!id) return;
+    const name = departmentNameRef.current.input.value;
+    const code = departmentCodeRef.current.input.value;
+    const url = `${getAPIHostName()}/departments/${id}`;
+
+    httpPut(url, { name, code }, accessToken)
+      .then(res => {
+        if (res.success) {
+          console.log('res :>> ', res);
+          // setDepartmentList(oldDepartmentList => [res.data, ...oldDepartmentList]);
+          notification.success({
+            title: 'Success',
+            message: 'Successfully updated department'
+          });
+          // setOpenUpSertDepartment(false);
+        }
+      })
+      .catch(() => {
+        notification.error({
+          title: 'Error',
+          message: 'Failed to update department'
+        });
+        // setOpenUpSertDepartment(false);
+      });
+  };
+
   const content = id => {
     return (
       <div className="action">
         <div className="action__edit">
           <EditOutlined />
-          <div onClick={() => {}}>Edit</div>
+          <div onClick={() => openModalUpSertDepartment(id)}>Edit</div>
         </div>
         <div
           className="action__delete"
@@ -63,7 +98,7 @@ export default function DepartmentPage() {
   };
 
   const showConfirm = idDelete => {
-    confirm({
+    Modal.confirm({
       title: 'Do you want to delete this department?',
       icon: <ExclamationCircleFilled />,
       onOk() {
@@ -123,6 +158,32 @@ export default function DepartmentPage() {
     }
   ];
 
+  const handleAddDepartment = () => {
+    const name = departmentNameRef.current.input.value;
+    const code = departmentCodeRef.current.input.value;
+    const url = `${getAPIHostName()}/departments`;
+
+    httpPost(url, { name, code }, accessToken)
+      .then(res => {
+        if (res.success) {
+          setDepartmentList(oldDepartmentList => [res.data, ...oldDepartmentList]);
+          notification.success({
+            title: 'Success',
+            message: 'Successfully created a new department'
+          });
+          setOpenUpSertDepartment(false);
+        }
+      })
+      .catch(err => {
+        notification.error({
+          title: 'Error',
+          message: 'Failed to create new department'
+        });
+        console.log('err :>> ', err);
+        setOpenUpSertDepartment(false);
+      });
+  };
+
   const getDataSource = () => {
     let departmentToRender = departmentList.filter(item => item.is_deleted);
 
@@ -140,7 +201,11 @@ export default function DepartmentPage() {
           onChange={e => setSearchValue(e.target.value)}
           className="department__search-inp"
         />
-        <Button className="department__search-btn" rightIcon={<PlusOutlined />}>
+        <Button
+          className="department__search-btn"
+          onClick={() => openModalUpSertDepartment()}
+          rightIcon={<PlusOutlined />}
+        >
           Add department
         </Button>
       </div>
@@ -151,6 +216,22 @@ export default function DepartmentPage() {
         rowKey={record => record._id}
         dataSource={getDataSource()}
       />
+
+      <Modal
+        title="Add department"
+        open={openUpSertDepartment}
+        onOk={() => {
+          updateId ? handleUpdateDepartment(updateId) : handleAddDepartment();
+        }}
+        wrapClassName="add__department-modal"
+        onCancel={() => setOpenUpSertDepartment(false)}
+        okText="Add"
+      >
+        <div className="add__department-label">Name:</div>
+        <CustomInput ref={departmentNameRef} placeholder="Enter department name" />
+        <div className="add__department-label">Code:</div>
+        <CustomInput ref={departmentCodeRef} placeholder="Enter department code" />
+      </Modal>
     </div>
   );
 }
