fd94ba39307ca5869eebdb87b3374b843732a2c2
Pham Tuan Duc <89022216+toughguy1010@users.noreply.github.com>
2023-03-07 20:42:50 +0700

    update-delete-action

--- src/pages/benefit/benefit-page.jsx
+++ src/pages/benefit/benefit-page.jsx
@@ -1,13 +1,16 @@
 import React, { useRef, useState } from 'react';
 import { useEffect } from 'react';
-import { Table, Popover, notification, Modal, Input, InputNumber, Select } from 'antd';
+import { Table, Popover, notification, Modal, Input, InputNumber, Select, } from 'antd';
 import Button from '../../components/button/button';
 import './benefit-page.scss';
-import { httpGet } from '../../services/request';
+import { httpGet, httpDelete, httpPost, httpPut } from '../../services/request';
 import { getAPIHostName } from '../../utils';
-import { useSetRecoilState } from 'recoil';
+import { useSetRecoilState, useRecoilValue } from 'recoil';
 import { loadingState } from '../../recoil/store/app';
 import CustomInput from '../../components/custom-input/custom-input';
+import { accessTokenState } from '../../recoil/store/account';
+import { PlusOutlined, MoreOutlined, EditOutlined, DeleteOutlined, ExclamationCircleFilled } from '@ant-design/icons';
+
 const { Search } = Input;
 const BenefitPage = () => {
   const [benefitList, setBenefitList] = useState([]);
@@ -15,13 +18,22 @@ const BenefitPage = () => {
   const [updateId, setUpdateId] = useState();
   const [openUpSertBenefit, setOpenUpSertBenefit] = useState(false);
   const benefitNameRef = useRef(null);
+  const benefitDescriptionRef = useRef(null);
+  const benefitStandardRef = useRef(null);
+  const benefitMonthRef = useRef(null);
+  const benefitStatusRef = useRef(null);
+  const [isLoadingTable, setIsLoadingTable] = useState(false);
+  const accessToken = useRecoilValue(accessTokenState);
+  const [searchValue, setSearchValue] = useState('');
+
+  // console.log(accessToken)
   const { Option } = Select;
   function handleChange(value) {
-    console.log(`Selected ${value}`);
+    // console.log(`Selected ${value}`);
   }
   function SelectComponent() {
     return (
-      <Select defaultValue="Active" style={{ width: 120 }} onChange={handleChange}>
+      <Select ref={benefitStatusRef} defaultValue="Active" style={{ width: 120 }} onChange={handleChange}>
         <Option value="Active">Active</Option>
         <Option value="Unactive">Unactive</Option>
       </Select>
@@ -57,23 +69,58 @@ const BenefitPage = () => {
     setUpdateId(id);
     setOpenUpSertBenefit(true);
   };
+  const showConfirm = (iDelete) => {
+    Modal.confirm({
+      title: 'Do you want to delete this department?',
+      icon: <ExclamationCircleFilled />,
+      onOk() {
+        setIsLoadingTable(true)
+        const url = `${getAPIHostName()}/benefits/${iDelete}`;
+        console.log(url);
+        httpDelete(url, accessToken)
+          .then(res => {
+            if (res.success) {
+              setBenefitList(oldBenefitList => oldBenefitList.filter(benefit => benefit._id != iDelete))
+              notification.success({
+                title: 'Success',
+                message: res.message || 'Delete deparment success'
+              });
+            }
+            setIsLoadingTable(false)
+          })
+          .catch(() => {
+            notification.error({
+              title: 'Error',
+              message: 'Delete deparment failed'
+            });
+            setIsLoadingTable(false);
+          })
+      },
+      onCancel() {
+        return;
+      }
+    })
+  }
   const handleUpdateBenefit = id => {
-    if (!id) return;
     const name = benefitNameRef.current.input.value;
+    const description = benefitDescriptionRef.current.input.value;
+    const standard = benefitStandardRef.current.value;
+    const month = benefitMonthRef.current.value;
+    const status = benefitStatusRef.current;
+    console.log('status', status)
   };
-  const content = id => (
-    <div className="benefit__action-menu">
-      <Button className={'benefit__button'} onClick={() => openModalUpSertBenefit(id)}>
-        Edit
-      </Button>
-      <Button className={'benefit__button'}>Mark as default</Button>
-      <Button className={'benefit__button'}>Deactive</Button>
-      <Button className={'benefit__button'} onClick={() => {}}>
-        Delete
-      </Button>
-    </div>
-  );
-
+  const content = (id) => {
+    return (
+      <div className="benefit__action-menu">
+        <Button className={'benefit__button'} onClick={() => openModalUpSertBenefit(id)}>
+          Edit
+        </Button>
+        <Button className={'benefit__button'} onClick={() => showConfirm(id)}>
+          Delete
+        </Button>
+      </div>
+    )
+  };
   const columns = [
     {
       title: 'Name',
@@ -105,7 +152,9 @@ const BenefitPage = () => {
       render: (_, item) => {
         return (
           <div className="benefit__action">
-            <Popover placement="topLeft" content={content(item._id)} trigger="click">
+            <Popover placement="topLeft" content={content(item._id)} trigger="click" onClick={() => {
+
+            }}>
               ...
             </Popover>
           </div>
@@ -113,9 +162,13 @@ const BenefitPage = () => {
       }
     }
   ];
-  const _benefitList = () => {
-    return benefitList;
+  const getDataSource = () => {
+    const benefitData = benefitList.filter(item => item.is_deleted === false)
+    return benefitData.filter(
+      item => item.name?.indexOf(searchValue) >= 0 || item.code?.indexOf(searchValue) >= 0
+    )
   };
+
   return (
     <div className="benefit__container">
       <div className="benefit__top">
@@ -124,7 +177,11 @@ const BenefitPage = () => {
         </div>
         <div className="benefit__top-right">
           <div className="benefit__top-search">
-            <Search className="benefit__top-search-input" placeholder="Type here to search" />
+            <Search
+              className="benefit__top-search-input"
+              placeholder="Type here to search"
+              onChange={e => setSearchValue(e.target.value)}
+            />
           </div>
           <div className="benefit__top-modal">
             <Button onClick={() => openModalUpSertBenefit()} className="benefit__top-modal-btn">
@@ -134,7 +191,7 @@ const BenefitPage = () => {
         </div>
       </div>
       <div className="benefit__center">
-        <Table columns={columns} dataSource={_benefitList()} pagination={false} rowKey={record => record._id}></Table>
+        <Table columns={columns} dataSource={getDataSource()} pagination={false} rowKey={record => record._id}></Table>
         <Modal
           title="Add benefit"
           open={openUpSertBenefit}
@@ -152,17 +209,17 @@ const BenefitPage = () => {
               </div>
               <div id="benefit__modal-description" className="benefit__modal-item">
                 <div className="benefit__modal-label">Description:</div>
-                <CustomInput placeholder="Enter description" />
+                <CustomInput ref={benefitDescriptionRef} placeholder="Enter description" />
               </div>
             </div>
             <div className="benefit__modal-right">
               <div id="benefit__modal-standard" className="benefit__modal-item">
                 <div className="benefit__modal-label">Standard Leave:</div>
-                <InputNumber style={{ width: 120 }} defaultValue={0} />
+                <InputNumber ref={benefitStandardRef} style={{ width: 120 }} defaultValue={0} />
               </div>
               <div id="benefit__modal-month" className="benefit__modal-item">
                 <div className="benefit__modal-label">Month:</div>
-                <InputNumber style={{ width: 120 }} min={1} defaultValue={1} />
+                <InputNumber ref={benefitMonthRef} style={{ width: 120 }} min={1} defaultValue={1} />
               </div>
               <div id="benefit__modal-status" className="benefit__modal-item">
                 <div className="benefit__modal-label">Status:</div>
