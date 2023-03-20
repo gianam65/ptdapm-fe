eb3e347ed4bf36217ee44368a2072879c5f197a0
TuanHiep <trinhtuanhiep1@gmail.com>
2023-03-20 22:58:35 +0700

    employee_crud

--- src/pages/employees/employees-page.jsx
+++ src/pages/employees/employees-page.jsx
@@ -1,7 +1,7 @@
 import './employees-page.scss';
-import { Table, notification, Modal, Popover, message, Upload } from 'antd';
+import { Table, notification, Modal, Popover, Upload, message, InputNumber, Select, Input, Pagination } from 'antd';
 import { useEffect, useState, useRef } from 'react';
-import { httpGet, httpPost } from '../../services/request';
+import { httpGet, httpPost, httpDelete } from '../../services/request';
 import { getAPIHostName } from '../../utils';
 import { loadingState } from '../../recoil/store/app';
 import { useSetRecoilState, useRecoilValue } from 'recoil';
@@ -10,7 +10,9 @@ import { CSVLink } from 'react-csv';
 import { CloudDownloadOutlined } from '@ant-design/icons';
 import CustomInput from '../../components/custom-input/custom-input';
 import { accessTokenState } from '../../recoil/store/account';
-import {  MoreOutlined  } from '@ant-design/icons';
+import { MoreOutlined, PlusOutlined, EditOutlined, DeleteOutlined } from '@ant-design/icons';
+import Button from '../../components/button/button';
+import Search from 'antd/es/transfer/search';
 
 const HEADERS = [
   { label: 'Tên nhân viên', key: 'name' },
@@ -25,9 +27,16 @@ const HEADERS = [
 ];
 
 const EmployeesPage = () => {
+  const { Option } = Select;
+  const [departmentList, setDepartmentList] = useState([]);
+  const [benefitList, setBenefitList] = useState([]);
+  const [activePage, setActivePage] = useState(1)
+  const [totalPage, setTotalPage] = useState()
   const [listEmployees, setListEmployees] = useState([]);
   const setPageLoading = useSetRecoilState(loadingState);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [editModalOpen, setEditModalOpen] = useState(false);
+  const [deleteModalOpen, setDeleteModalOpen] = useState(false);
   const accessToken = useRecoilValue(accessTokenState);
   const [imageUrl, setImageUrl] = useState();
   const employeesNameRef = useRef(null);
@@ -36,46 +45,107 @@ const EmployeesPage = () => {
   const employeesBirthdayRef = useRef(null);
   const employeesPhoneRef = useRef(null);
   const employeesGenderRef = useRef(null);
-
+  const employeesAddressRef = useRef(null)
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [code, setCode] = useState("")
+  const [birthday, setBirthday] = useState("")
+  const [phone, setPhone] = useState("")
+  const [gender, setGender] = useState("")
+  const [address, setAddress] = useState("")
+  const [salaryRanks, setSalaryRanks] = useState(1)
+  const [department, setDepartment] = useState()
+  const [benefit, setBenefit] = useState()
+  const [startDate, setStartDate] = useState()
+  const [id, setID] = useState()
+  const [modalText] = useState('Are you sure to delete this employee ?');
+  const [textSearch, setTextSearch] = useState()
+  const [totalEmployee, setTotalEmployee] = useState()
+  const { Search } = Input
   useEffect(() => {
-    const fetchEmployees = () => {
-      setPageLoading(true);
-      const url = `${getAPIHostName()}/employees`;
-      httpGet(url)
-        .then(res => {
-          console.log(res, 'dataaa')
-          if (res.success) {
-            const { employeeList } = res.data;
-            setListEmployees(employeeList);
-          }
-          setPageLoading(false);
-        })
-        .catch(() => {
-          setPageLoading(false);
-        });
-    };
-    fetchEmployees();
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    fetchEmployees(activePage);
+    getDepartment();
+    getBenefit();
   }, []);
+
+  const getBenefit = () => {
+    const url = `${getAPIHostName()}/benefits`;
+    setPageLoading(true);
+    httpGet(url)
+      .then(res => {
+        if (res.success) {
+          const { benefitList } = res.data;
+          setBenefitList(benefitList);
+        }
+        setPageLoading(false);
+      })
+      .catch(() => {
+        notification.error({
+          title: 'Error',
+          message: 'Can not get benefit data'
+        });
+        setPageLoading(false);
+      });
+  };
+
+  const getDepartment = () => {
+    const url = `${getAPIHostName()}/departments`;
+    setPageLoading(true);
+    httpGet(url)
+      .then(res => {
+        if (res.success) {
+          const { departmentList } = res.data;
+          setDepartmentList(departmentList);
+        }
+        setPageLoading(false);
+      })
+      .catch(() => {
+        notification.error({
+          title: 'Lỗi',
+          message: 'Không thể lấy thông tin phòng ban'
+        });
+        setPageLoading(false);
+      });
+  };
+
+  const fetchEmployees = (activePage) => {
+    setPageLoading(true);
+    const url = `${getAPIHostName()}/employees?is_deleted=false&limit=4&page=${activePage}`;
+    httpGet(url)
+      .then(res => {
+        if (res.success) {
+          const { employeeList, totalPage, totalEmployee } = res.data;
+          console.log(res.data, "1222")
+          setListEmployees(employeeList);
+          setActivePage(activePage)
+          setTotalPage(totalPage)
+          setTotalEmployee(totalEmployee)
+        }
+        setPageLoading(false);
+      })
+      .catch(() => {
+        setPageLoading(false);
+      });
+  };
 
   const fetchAddEmployees = () => {
     const name = employeesNameRef.current.input.value;
-    const code = employeesCodeRef.current.input.value;
+    const codeEmployee = employeesCodeRef.current.input.value;
     const email = employeesEmailRef.current.input.value;
     const birthday = employeesBirthdayRef.current.input.value;
-    const phone = employeesPhoneRef.current.input.value;
+    const phoneNumber = employeesPhoneRef.current.input.value;
     const gender = employeesGenderRef.current.input.value;
+    const address = employeesAddressRef.current.input.value;
     setPageLoading(true);
     const url = `${getAPIHostName()}/employees`;
-    httpPost(url, {name, code, email, birthday, phone, gender}, accessToken)
+    httpPost(url, { name, codeEmployee, email, birthday, phoneNumber, gender, address, salaryRanks }, accessToken)
       .then(res => {
         if (res.success) {
-          setListEmployees(oldEmployeeList => [res.data, ...oldEmployeeList]);
           notification.success({
             title: 'Success',
             message: 'Successfully created a new employee'
           });
+          fetchEmployees()
         } else {
           notification.error({
             title: 'Error',
@@ -89,6 +159,95 @@ const EmployeesPage = () => {
       });
   };
 
+  const handleCodeEmployee = codeEmployee => {
+    setCode(codeEmployee)
+    setEditModalOpen(true)
+  }
+
+  const fetchUpdateEmployees = () => {
+    setPageLoading(true);
+    const url = `${getAPIHostName()}/employees?department=&position=&benefit=`;
+    let buildBodyToUpdate = {
+      name: name,
+      email: email,
+      codeEmployee: code,
+      phoneNumber: phone,
+      gender: gender,
+      address: address,
+      birthday: birthday,
+      deparment: department,
+      benefit: benefit,
+      salaryRanks: salaryRanks,
+      startDate: startDate
+    };
+    httpPost(url, buildBodyToUpdate, accessToken)
+      .then(res => {
+        if (res.success) {
+          notification.success({
+            title: 'Success',
+            message: 'Successfully update a new employee'
+          })
+          setEditModalOpen(false)
+          fetchEmployees()
+        } else {
+          notification.error({
+            title: 'Error',
+            message: res.message || 'Failed to update new employee'
+          });
+        }
+        setPageLoading(false);
+      })
+      .catch(() => {
+        setPageLoading(false);
+      });
+  };
+
+  const fetchDeleteEmployees = () => {
+    setPageLoading(true);
+    const url = `${getAPIHostName()}/employees/delete/${id}`;
+    console.log(url, "12121")
+    console.log(accessToken)
+    httpDelete(url, accessToken)
+      .then(res => {
+        if (res.success) {
+          notification.success({
+            title: 'Success',
+            message: 'Successfully delete an employee',
+          });
+          fetchEmployees()
+        } else {
+          notification.error({
+            title: 'Error',
+            message: res.message || 'Failed to delete an employee'
+          });
+        }
+        setPageLoading(false);
+      })
+      .catch(() => {
+        setPageLoading(false);
+      });
+  };
+
+  const fetchSearchEmployees = (activePage) => {
+    setPageLoading(true);
+    const url = `${getAPIHostName()}/employees?text=${textSearch}&is_deleted=false&page=${activePage}`;
+    httpGet(url, accessToken)
+      .then(res => {
+        if (res.success) {
+          const {employeeList, totalPage} = res.data
+          setListEmployees(employeeList)
+          setTotalPage(totalPage)
+        } else {
+          notification.error({
+            title: 'Error',
+          });
+        }
+        setPageLoading(false);
+      })
+      .catch(() => {
+        setPageLoading(false);
+      });
+  };
 
   const columns = [
     {
@@ -156,13 +315,33 @@ const EmployeesPage = () => {
       title: 'ACTION',
       render: (_, item) => (
         <div className="employees__row-action">
-          <Popover  trigger="click">
+          <Popover content={content(item.codeEmployee, item._id)} trigger="click">
             <MoreOutlined />
           </Popover>
         </div>
       )
     }
   ];
+
+  const handleID = id => {
+    setID(id)
+    setDeleteModalOpen(true)
+  }
+
+  const content = (codeEmployee, id) => {
+    return (
+      <div className="action manipulated__action">
+        <div onClick={() => handleCodeEmployee(codeEmployee)} className="action__edit">
+          <EditOutlined />
+          <div>Edit</div>
+        </div>
+        <div onClick={() => handleID(id)} className="action__delete">
+          <DeleteOutlined />
+          <div>Xoá</div>
+        </div>
+      </div>
+    );
+  };
 
   const buildDataToExport = () => {
     let data = [];
@@ -186,6 +365,34 @@ const EmployeesPage = () => {
     return data;
   };
 
+  const beforeUpload = (file) => {
+    const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
+    if (!isJpgOrPng) {
+      message.error('You can only upload JPG/PNG file!');
+    }
+    const isLt2M = file.size / 1024 / 1024 < 2;
+    if (!isLt2M) {
+      message.error('Image must smaller than 2MB!');
+    }
+    return isJpgOrPng && isLt2M;
+  };
+
+  const uploadButton = (
+    <div>
+      {<PlusOutlined />}
+      <div
+        style={{
+          marginTop: 8,
+        }}
+      >
+        Upload
+      </div>
+    </div>
+  );
+
+
+
+
   return (
     <div className="employess__section">
       <CSVLink data={buildDataToExport()} headers={HEADERS}>
@@ -194,16 +401,36 @@ const EmployeesPage = () => {
           <CloudDownloadOutlined />
         </div>
       </CSVLink>
+      <div className="employees__action">
+        <Search
+          type="search"
+          style={{
+            width: 200,
+          }}
+          placeholder="Nhập vào đây để tìm kiếm"
+          className="employees__search-inp"
+          onChange={e => setTextSearch(e.target.value)}
+          onSearch={() => textSearch ? fetchSearchEmployees() : fetchEmployees()}
+        />
+        <Button
+          className="employees__search-btn"
+          rightIcon={<PlusOutlined />}
+          onClick={() => setIsModalOpen(true)}
+        >
+          Thêm nhân viên
+        </Button>
+      </div>
       <div className="employees__container">
-        <Table columns={columns} dataSource={listEmployees} rowKey={record => record._id} pagination={true} />
+        <Table columns={columns} dataSource={listEmployees} rowKey={record => record._id} pagination={{total: totalEmployee, current: activePage, pageSize: 4, onChange: activePage => fetchEmployees(activePage)}}  />
       </div>
       <Modal
+        style={{ width: 1000 }}
         title="Add employees"
         wrapClassName="add__employees-modal"
         okText="Add"
         open={isModalOpen}
         onOk={() => fetchAddEmployees()}
-        onCancel={handleCancel}
+        onCancel={() => setIsModalOpen(false)}
       >
         <div style={{ display: "flex", justifyContent: "space-between" }}>
           <div className='add__employees_left'>
@@ -221,11 +448,33 @@ const EmployeesPage = () => {
             <CustomInput ref={employeesGenderRef} placeholder="Male or Female" />
             <div className="add__employees-label">Employee code :</div>
             <CustomInput ref={employeesCodeRef} placeholder="Enter employees code" />
+            <div className="add__employees-label">Address: </div>
+            <CustomInput ref={employeesAddressRef} placeholder="Address" />
+            <div className="add__employees-label">salaryRank: </div>
+            <InputNumber type={'number'} defaultValue={salaryRanks} onChange={(value) => setSalaryRanks(value)} />
+            <Select placeholder="Depratment"
+              style={{ width: 120 }}
+            >
+              {departmentList.map((list, idx) => {
+                return (
+                  <Option key={idx} value={list._id}>{list.name}</Option>
+                )
+              })}
+            </Select>
+            <Select placeholder="Benefit"
+              style={{ width: 120 }}>
+              {benefitList.map((list, idx) => {
+                return (
+                  <Option key={idx} value={list._id}>{list.name}</Option>
+                )
+              })}
+            </Select>
             <Upload
               name="avatar"
               listType="picture-circle"
               className="avatar-uploader"
               showUploadList={false}
+              beforeUpload={beforeUpload}
             >
               {imageUrl ? (
                 <img
@@ -241,6 +490,76 @@ const EmployeesPage = () => {
             </Upload>
           </div>
         </div>
+      </Modal>
+
+      <Modal title="Update Employees" open={editModalOpen} onOk={() => fetchUpdateEmployees()} onCancel={() => setEditModalOpen(false)}>
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <div className='add__employees_left'>
+            <div className="add__employees-label">Name:</div>
+            <CustomInput onChange={e => setName(e.target.value)} placeholder="Enter employees name" />
+            <div className="add__employees-label">Email:</div>
+            <CustomInput onChange={e => setEmail(e.target.value)} placeholder="Enter employees email" />
+            <div className="add__employees-label">Your birthday :</div>
+            <CustomInput onChange={e => setBirthday(e.target.value)} placeholder="Your birthday " />
+            <div className="add__employees-label">phoneNumber:</div>
+            <CustomInput onChange={e => setPhone(e.target.value)} placeholder="Your number" />
+            <div className="add__employees-label">startDate:</div>
+            <CustomInput onChange={e => setStartDate(e.target.value)} placeholder="Your number" />
+          </div>
+          <div className='add__employees_right'>
+            <div className="add__employees-label">Gender:</div>
+            <CustomInput onChange={e => setGender(e.target.value)} placeholder="Male or Female" />
+            <div className="add__employees-label">Address: </div>
+            <CustomInput onChange={e => setAddress(e.target.value)} placeholder="Address" />
+            <div className="add__employees-label">salaryRank: </div>
+            <InputNumber onChange={value => setSalaryRanks(value)} type={'number'} defaultValue={salaryRanks} />
+            <Select onChange={value => setDepartment(value)} placeholder="Depratment" style={{ width: 120 }}
+            >
+              {departmentList.map((list, idx) => {
+                return (
+                  <Option key={idx} value={list._id}>{list.name}</Option>
+                )
+              })}
+            </Select>
+            <Select onChange={value => setBenefit(value)} placeholder="Benefit"
+              style={{ width: 120 }}>
+              {benefitList.map((list, idx) => {
+                return (
+                  <Option key={idx} value={list._id}>{list.name}</Option>
+                )
+              })}
+            </Select>
+            <Upload
+              name="avatar"
+              listType="picture-circle"
+              className="avatar-uploader"
+              showUploadList={false}
+              beforeUpload={beforeUpload}
+            >
+              {imageUrl ? (
+                <img
+                  src={imageUrl}
+                  alt="avatar"
+                  style={{
+                    width: '100%',
+                  }}
+                />
+              ) : (
+                uploadButton
+              )}
+            </Upload>
+          </div>
+        </div>
+      </Modal>
+      <Modal
+        title="delete employees"
+        wrapClassName="delete__employees-modal"
+        okText="delete"
+        open={deleteModalOpen}
+        onOk={() => fetchDeleteEmployees()}
+        onCancel={() => setDeleteModalOpen(false)}
+      >
+        <p>{modalText}</p>
       </Modal>
     </div>
   );
